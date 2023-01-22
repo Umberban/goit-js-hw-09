9ad95233b27ff226feefd3a6d04d16cfbd71da4d
Umberban <110768888+Umberban@users.noreply.github.com>
2023-01-22 11:25:59 +0000

    Deploying to gh-pages from @ Umberban/goit-js-hw-09@4a0981a8e13844e78eb65bc3d6cec004c2f19529 🚀

---
+++ 01-color-switcher.471aba23.js
@@ -0,0 +1,2 @@
+!function(){var e=document.querySelector("button[data-start]"),t=document.querySelector("button[data-stop]"),o=document.querySelector("body"),n=null;e.addEventListener("click",(function(){n=setInterval((function(){o.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16))}),1e3),t.disabled=!1,e.disabled=!0,disableChange()})),t.addEventListener("click",(function(){clearInterval(n),e.disabled=!1,t.disabled=!0})),console.log(t),console.log(e)}();
+//# sourceMappingURL=01-color-switcher.471aba23.js.map
